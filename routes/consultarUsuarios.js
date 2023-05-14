ed7781505111cff11d8c74217cec843bfe70f391
Angel Juarez <spotify27816@gmail.com>
2023-05-14 00:16:30 -0600

    Se agrego la ruta para consultar Usuarios

---
+++ routes/consultarUsuarios.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const route = express();
+const usuarios = require('../models/user');
+
+route.get('/usuarios',async (req,res) =>{
+
+    const prov = await usuarios.find().then(function(data){
+        if(data){
+            res.json({
+                data
+            })
+        }
+    }).catch(function(err){
+        if(err){
+            return res.json({
+                status:404,
+                mensaje:"error",
+                err
+            })
+    }});
+
+});
+
+module.exports = route;
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(require('./consultarNotas'));
+app.use(require('./consultarUsuarios'));
 
 module.exports = app;
