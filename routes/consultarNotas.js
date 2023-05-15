876d43317d8504ae13ae23c8259a18c5eee590de
Angel Juarez <spotify27816@gmail.com>
2023-05-14 22:25:32 -0600

    Ruta para consultar todas la notas terminada

--- routes/consultarNotas.js
+++ routes/consultarNotas.js
@@ -1,9 +1,25 @@
 const express  = require("express");
-
 const route = express();
+const notas = require('../models/nota');
 
-route.get('/consultarNota',(req,res)=>{
-    res.send('Mostrando todas las notas ')
-})
+
+route.get('/notas',async (req,res) =>{
+
+    const prov = await notas.find().then(function(data){
+        if(data){
+            res.json({
+                data
+            })
+        }
+    }).catch(function(err){
+        if(err){
+            return res.json({
+                status:404,
+                mensaje:"error",
+                err
+            })
+    }});
+
+});
 
 module.exports = route
