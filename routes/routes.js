0542ca736c322cf28b4318382877bd266371954c
Angel Juarez <spotify27816@gmail.com>
2023-05-16 14:16:24 -0600

    Ruta buscar usuario por correo , terminada

--- routes/routes.js
+++ routes/routes.js
@@ -14,6 +14,7 @@ app.use(require('../routes/actualizarNota'));
 app.use(require('../routes/borrarNotasUsuario'));
 app.use(require('../routes/buscarUsuario'));
 app.use(require('./actualizarEstado'));
+app.use(require('./buscarUsuarioPorCorreo'));
 //borrar
 app.use(require('../routes/borrarNota'));
 
